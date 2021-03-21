69d266a4a316b5ddfb375c9c230ce6be3c24512d
vikzh <vikt.anmail@gmail.com>
2021-03-21 19:58:36 +0300

    basic api added

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from posts.models import Post
+from posts.serializers import PostSerializer
+from rest_framework import generics
 
-# Create your views here.
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
